561b5529df940bf396b4369fc7008957f54c1209
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-08 08:48:23 +0530

    Understood the flow of create-react-app made react

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+// App is just a method which return HTML
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <h1>Classic react app from create-react-app</h1>;
 }
 
 export default App;
