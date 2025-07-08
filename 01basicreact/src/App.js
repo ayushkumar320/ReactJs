108b61393ba3cbaefc5747fe8fc807e9cc33fe2f
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-08 09:18:14 +0530

    imported a youtube function in App.js

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,7 +1,12 @@
 // App is just a method which return HTML
-
+import Youtube from "./Youtube.jsx";
 function App() {
-  return <h1>Classic react app from create-react-app</h1>;
+  return (
+    <>
+      <h1>Classic react app from create-react-app</h1>
+      <Youtube />
+    </>
+  );
 }
 
 export default App;
