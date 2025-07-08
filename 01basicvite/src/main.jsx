12955d34e7eb42d5c498b01ef69a8e92e1ffa719
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-08 14:22:55 +0530

    updated main.jsx

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -5,8 +5,30 @@ import { createRoot } from 'react-dom/client' // Similar to above we can import 
 import App from './App.jsx'
 import Youtube from "./Youtube.jsx"
 
+const reactElement = {
+  type: "a", // tag like <a>, or <h1>
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google",
+};
+
+function MyApp() {
+    return (
+      <>
+        <h1>Custom React App</h1> 
+    	</>
+    )
+}
+
+const AnotherElement = (
+	<a href = "https://google.com" target = "_blank">Visit Google</a>
+)
+
+
 createRoot(document.getElementById('root')).render(
   
-    <App />
-
+    // <App />
+		 AnotherElement
 )
