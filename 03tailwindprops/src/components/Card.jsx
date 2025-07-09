81dfe16e9d109d043215afa865a695314f5e0d0d
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-09 13:26:54 +0530

    integrate tailwind wth Vite

---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Card() {
+  return (
+    <>
+      <div className="bg-orange-500 font-medium rounded-md p-3 mt-5">Card with Vite and Tailwind</div>
+      <img src="https://images.pexels.com/photos/1616397/pexels-photo-1616397.jpeg" alt="" className="w-64 rounded-xl mt-10"/>
+    </>
+  );
+}
+
+export default Card;
